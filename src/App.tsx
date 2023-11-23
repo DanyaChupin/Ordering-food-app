3cd25d2909e1aedbdbcca753c327ed4d14a6ab71
DanyaChupin <chupa.chupa.14@mail.ru>
2023-11-23 14:43:09 +0300

    Создание проекта

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Button from '../components/Button/Button'
+import './App.css'
+
+function App() {
+	return (
+		<div>
+			<Button onClick={() => console.log('hello')}>Hello</Button>
+		</div>
+	)
+}
+
+export default App
