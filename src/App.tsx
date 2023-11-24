fba1f7264026a6daae2232f3421d09796b7ed71b
DanyaChupin <chupa.chupa.14@mail.ru>
2023-11-24 12:36:03 +0300

    Создание компонентов Button, Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Button from '../components/Button/Button'
-
+import Input from '../components/Input/Input'
 function App() {
 	return (
 		<div>
 			<Button onClick={() => console.log('hello')}>Hello</Button>
+
+			<Button appearence='big' onClick={() => console.log('hello')}>
+				Hello
+			</Button>
+			<Input placeholder='Email' />
 		</div>
 	)
 }
