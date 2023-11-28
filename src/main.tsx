ceec167de751712c4e5ea79d40592fb8e839ec1a
DanyaChupin <chupa.chupa.14@mail.ru>
2023-11-28 13:48:57 +0300

    Загрузка страницы Menu путем lazy, подгрузка продука с помощью async defer

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom'
 import { Cart } from './pages/cart/Cart.tsx'
-import Menu from './pages/Menu/Menu.tsx'
 import { Layout } from './layout/Menu/Layout.tsx'
 import { Error } from './pages/Error/Error.tsx'
 import Product from './pages/Product/Product.tsx'
 import axios from 'axios'
 import { PREFIX } from './helpers/API.ts'
 import './index.css'
+
+const Menu = lazy(() => import('./pages/Menu/Menu.tsx'))
 
 const router = createBrowserRouter([
 	{
@@ -17,7 +18,11 @@ const router = createBrowserRouter([
 		children: [
 			{
 				path: '/',
-				element: <Menu />,
+				element: (
+					<Suspense fallback={<>Загрузка...</>}>
+						<Menu />
+					</Suspense>
+				),
 			},
 			{
 				path: '/cart',
@@ -28,14 +33,12 @@ const router = createBrowserRouter([
 				element: <Product />,
 				errorElement: <>Ошибка</>,
 				loader: async ({ params }) => {
-					await new Promise<void>(resolve => {
-						setTimeout(() => {
-							resolve()
-						}, 2000)
+					return defer({
+						data: axios
+							.get(`${PREFIX}/products/${params.id}`)
+							.then(data => data)
+							.catch(e => alert(e)),
 					})
-					const { data } = await axios.get(`${PREFIX}/product1s/${params.id}`)
-
-					return data
 				},
 			},
 		],
--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,9 +1,16 @@
-import { useLoaderData } from 'react-router-dom'
+import { Await, useLoaderData } from 'react-router-dom'
 import { Product } from '../../interfaces/product.interface'
+import { Suspense } from 'react'
 
 const Product = () => {
-	const data = useLoaderData() as Product
-	return <div>{data.name}</div>
+	const data = useLoaderData() as { data: Product }
+	return (
+		<Suspense fallback={'Загружа...'}>
+			<Await resolve={data.data}>
+				{({ data }: { data: Product }) => <>{data.name}</>}
+			</Await>
+		</Suspense>
+	)
 }
 
 export default Product
