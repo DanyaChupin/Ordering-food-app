f0b866549da0446c3a10717f1d4d24d233da58ba
DanyaChupin <chupa.chupa.14@mail.ru>
2023-12-06 15:35:16 +0300

    main.tsx - рефакторинг

--- src/main.tsx
+++ src/main.tsx
@@ -1,82 +1,10 @@
-import React, { Suspense, lazy } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter, defer } from 'react-router-dom'
-import Cart from './pages/Cart/Cart.tsx'
-import Layout from './layout/Menu/Layout.tsx'
-import { Error } from './pages/Error/Error.tsx'
-import Product from './pages/Product/Product.tsx'
-import axios from 'axios'
-import { PREFIX } from './helpers/API.ts'
+import { RouterProvider } from 'react-router-dom'
 import './index.css'
-import AuthLayout from './layout/Auth/AuthLayout.tsx'
-import Login from './pages/Login/Login.tsx'
-import Register from './pages/Register/Register.tsx'
-import { RequireAuth } from './helpers/RequireAuth.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store/store.ts'
-import SuccessOrder from './pages/SuccessOrder/SuccessOrder.tsx'
-
-const Menu = lazy(() => import('./pages/Menu/Menu.tsx'))
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: (
-			<RequireAuth>
-				<Layout />
-			</RequireAuth>
-		),
-		children: [
-			{
-				path: '/',
-				element: (
-					<Suspense fallback={<>Загрузка...</>}>
-						<Menu />
-					</Suspense>
-				),
-			},
-			{
-				path: '/cart',
-				element: <Cart />,
-			},
-			{
-				path: '/success/:id',
-				element: <SuccessOrder />,
-			},
-			{
-				path: '/product/:id',
-				element: <Product />,
-				errorElement: <>Ошибка</>,
-				loader: async ({ params }) => {
-					return defer({
-						data: axios
-							.get(`${PREFIX}/products/${params.id}`)
-							.then(data => data)
-							.catch(e => alert(e)),
-					})
-				},
-			},
-		],
-	},
-	{
-		path: '/auth',
-		element: <AuthLayout />,
-		children: [
-			{
-				path: 'login',
-				element: <Login />,
-			},
-			{
-				path: 'register',
-				element: <Register />,
-			},
-		],
-	},
-	{
-		path: '*',
-		element: <Error />,
-	},
-])
+import { router } from './routes/routes.data.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
---
+++ src/routes/routes.data.tsx
@@ -0,0 +1,75 @@
+import { RequireAuth } from '../helpers/RequireAuth'
+import { createBrowserRouter, defer } from 'react-router-dom'
+import Layout from '../layout/Menu/Layout'
+import { Suspense, lazy } from 'react'
+import Cart from '../pages/Cart/Cart'
+import SuccessOrder from '../pages/SuccessOrder/SuccessOrder'
+import Product from '../pages/Product/Product'
+import axios from 'axios'
+import { PREFIX } from '../helpers/API'
+import AuthLayout from '../layout/Auth/AuthLayout'
+import Login from '../pages/Login/Login'
+import Register from '../pages/Register/Register'
+import { Error } from '../pages/Error/Error'
+
+const Menu = lazy(() => import('../pages/Menu/Menu.tsx'))
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: (
+			<RequireAuth>
+				<Layout />
+			</RequireAuth>
+		),
+		children: [
+			{
+				path: '/',
+				element: (
+					<Suspense fallback={<>Загрузка...</>}>
+						<Menu />
+					</Suspense>
+				),
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+			{
+				path: '/success/:id',
+				element: <SuccessOrder />,
+			},
+			{
+				path: '/product/:id',
+				element: <Product />,
+				errorElement: <>Ошибка</>,
+				loader: async ({ params }) => {
+					return defer({
+						data: axios
+							.get(`${PREFIX}/products/${params.id}`)
+							.then(data => data)
+							.catch(e => alert(e)),
+					})
+				},
+			},
+		],
+	},
+	{
+		path: '/auth',
+		element: <AuthLayout />,
+		children: [
+			{
+				path: 'login',
+				element: <Login />,
+			},
+			{
+				path: 'register',
+				element: <Register />,
+			},
+		],
+	},
+	{
+		path: '*',
+		element: <Error />,
+	},
+])
